b5d235bef4ceb89c7971d7450fa08d572b94c99b
Syed Sibteali Baqar <sibteali786@gmail.com>
2023-11-27 01:38:44 +0500

    installed new tools

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import express from "express";
+import router from "./router";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   console.log("hello from express");
   res.status(200);
   res.json({ message: "hello" });
 });
+
+app.get("/", (req, res) => {
+  console.log("hello from express");
+  res.status(200);
+  res.json({ message: "hello" });
+});
+
+app.use("/api", router);
 
 export default app;
