00e31e69e431dfc8f16362542d7fe66d6f2cb4a8
Syed Sibteali Baqar <sibteali786@gmail.com>
2023-11-28 14:18:59 +0500

    added auth

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import router from "./router";
 import morgan from "morgan";
+import cors from "cors";
+import { protect } from "./auth";
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   console.log("hello from express");
@@ -18,6 +23,6 @@ app.get("/", (req, res) => {
   res.json({ message: "hello" });
 });
 
-app.use("/api", router);
+app.use("/api", protect, router);
 
 export default app;
