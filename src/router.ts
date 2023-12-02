9ae65c168da72237a879fd3197efd02db661f630
Syed Sibteali Baqar <sibteali786@gmail.com>
2023-12-02 08:46:37 +0500

    added validation for the some routes

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -8,14 +9,33 @@ router.get("/product", (req, res) => {
   res.json({ message: "Hello" });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
+router.put("/product/:id", body("name").isString(), (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400);
+    res.json({ errors: errors.array() });
+  }
+});
 router.post("/product", () => {});
 router.delete("/product/:id", () => {});
 
 // Update
 router.get("/update", () => {});
 router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
+router.put(
+  "/update/:id",
+  body("title").isString(),
+  body("body").isString(),
+  body("asset").optional().isString(),
+  body("version").optional().isString(),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400);
+      res.json({ errors: errors.array() });
+    }
+  },
+);
 router.post("/update", () => {});
 router.delete("/update/:id", () => {});
 
